4cb03777aa9884f94c9bc2b6b93ecbaa9ae175c5
maksimcoder <codeplanetio@gmail.com>
2025-01-20 17:10:37 +0300

    test 2

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import { MyButton } from "./components/Button/Button";
+
+export { MyButton }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
+/// <reference types="vite/client" />
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { globSync } from 'glob'
+import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts({
+    tsconfigPath: "./tsconfig.app.json",
+  })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        globSync(['src/components/**/index.tsx', 'src/main.ts']).map((file) => {
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          const entryName = path.relative(
+            'src',
+            file.slice(0, file.length - path.extname(file).length)
+          )
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url))
+          return [entryName, entryUrl]
+        })
+      ),
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name][extname]',
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
 })
